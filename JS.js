b30df1ba33f0ad3a0153a5b01da5eb4af8ca4b93
Kiyomarthi <a.kiyomarthi@gmail.com>
2024-06-26 13:27:47 +0330

    feat: add clock

--- JS.js
+++ JS.js
@@ -1,1 +1,17 @@
-console.log("hello");
+const hourHand = document.querySelector(".hour-hand");
+const minHand = document.querySelector(".min-hand");
+const secHand = document.querySelector(".second-hand");
+function handleClock() {
+  const clock = new Date();
+  const hour = clock.getHours();
+  const min = clock.getMinutes();
+  const sec = clock.getSeconds();
+
+  hourHand.style.transform = `rotate(${90 + hour * 30}deg)`;
+  minHand.style.transform = `rotate(${90 + min * 6}deg)`;
+
+  const secDeg = (sec / 60) * 360 + 90;
+  secHand.style.transform = `rotate(${secDeg}deg)`;
+}
+
+setInterval(handleClock, 1000);
